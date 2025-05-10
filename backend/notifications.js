8c610afb3111a132ef16946b61b8f606f5bb68cd
sukhpreetsvirdi <sukhpreetsvirdi@gmail.com>
2025-05-11 03:09:00 +0530

    dark mode added & footer modified , link to the sign-up page from service cards

--- backend/notifications.js
+++ backend/notifications.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
-const twilio = require("twilio");
+// require("dotenv").config();
+// const twilio = require("twilio");
 
-const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+// const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-function sendSMSNotification(message) {
-    client.messages.create({
-        body: message,
-        from: process.env.TWILIO_PHONE_NUMBER,
-        to: process.env.ADMIN_PHONE_NUMBER
-    }).then(() => console.log("📲 SMS sent successfully!"))
-      .catch(err => console.error("⚠ SMS Error:", err));
-}
+// function sendSMSNotification(message) {
+//     client.messages.create({
+//         body: message,
+//         from: process.env.TWILIO_PHONE_NUMBER,
+//         to: process.env.ADMIN_PHONE_NUMBER
+//     }).then(() => console.log("📲 SMS sent successfully!"))
+//       .catch(err => console.error("⚠ SMS Error:", err));
+// }
 
-module.exports = sendSMSNotification; // ✅ Make sure this is exporting the function
+// module.exports = sendSMSNotification; // ✅ Make sure this is exporting the function
